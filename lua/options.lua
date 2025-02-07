d0323183370ec04e0b4fd18fdcaadb982ae1af9d
Hebbar <prajwalhebbaras@gmail.com>
2025-02-07 09:47:19 +0530

    udpate snacks config for nvim

--- lua/options.lua
+++ lua/options.lua
@@ -9,9 +9,9 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- Sets tab width to 4 spaces, converts tabs to spaces, enables auto and smart indentation & enables line wrapping.
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
