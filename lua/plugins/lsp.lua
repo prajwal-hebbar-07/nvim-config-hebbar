0e5448e8ce05833df986591ef22c0fc56ffc06ec
Hebbar <prajwalhebbaras@gmail.com>
2025-02-10 19:15:15 +0530

    lsp config added for nvim

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,42 @@
+return {
+	-- LSP Configuration
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local servers = {
+			"html",
+			"cssls",
+			"tsserver",
+			"eslint",
+			"jsonls",
+			"gopls",
+			"rust_analyzer",
+			"dockerls",
+			"marksman",
+			"sqlls",
+			"yamlls",
+			"lua_ls",
+			"tailwindcss",
+			"bashls",
+			"pyright",
+			"solidity",
+			"solidity_ls_nomicfoundation",
+		}
+
+		require("mason-lspconfig").setup({
+			ensure_installed = servers,
+			automatic_installation = true,
+		})
+
+		for _, lsp in ipairs(servers) do
+			lspconfig[lsp].setup({
+				capabilities = capabilities,
+			})
+		end
+	end,
+}
