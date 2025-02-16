d03724beb870f2d8fbd0eda703e86dbd0e0b8c01
Hebbar <prajwalhebbaras@gmail.com>
2025-02-16 12:15:00 +0530

    update lsp related keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -52,3 +52,59 @@ vim.keymap.set(
 	'"+P',
 	{ noremap = true, silent = true, desc = "Paste before cursor from system clipboard" }
 )
+
+-- LSP (if installed)
+vim.keymap.set(
+	"n",
+	"gd",
+	"<cmd>lua vim.lsp.buf.definition()<CR>",
+	{ noremap = true, silent = true, desc = "Go to definition" }
+)
+vim.keymap.set(
+	"n",
+	"gD",
+	"<cmd>lua vim.lsp.buf.declaration()<CR>",
+	{ noremap = true, silent = true, desc = "Go to declaration" }
+)
+vim.keymap.set(
+	"n",
+	"gi",
+	"<cmd>lua vim.lsp.buf.implementation()<CR>",
+	{ noremap = true, silent = true, desc = "Go to implementation" }
+)
+vim.keymap.set(
+	"n",
+	"gr",
+	"<cmd>lua vim.lsp.buf.references()<CR>",
+	{ noremap = true, silent = true, desc = "List references" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>rn",
+	"<cmd>lua vim.lsp.buf.rename()<CR>",
+	{ noremap = true, silent = true, desc = "Rename symbol" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>ca",
+	"<cmd>lua vim.lsp.buf.code_action()<CR>",
+	{ noremap = true, silent = true, desc = "Code actions" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>e",
+	"<cmd>lua vim.diagnostic.open_float()<CR>",
+	{ noremap = true, silent = true, desc = "Show diagnostics" }
+)
+vim.keymap.set(
+	"n",
+	"[d",
+	"<cmd>lua vim.diagnostic.goto_prev()<CR>",
+	{ noremap = true, silent = true, desc = "Previous diagnostic" }
+)
+vim.keymap.set(
+	"n",
+	"]d",
+	"<cmd>lua vim.diagnostic.goto_next()<CR>",
+	{ noremap = true, silent = true, desc = "Next diagnostic" }
+)
